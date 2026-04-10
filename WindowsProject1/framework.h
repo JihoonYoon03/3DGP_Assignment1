94affea95e2c8bcbc8d66ad0ecbed09ddc87ec2b
laptop-msi <wlgns7922@gmail.com>
2026-04-10 17:10:38 +0900

    초기 커밋.

---
+++ WindowsProject1/framework.h
@@ -0,0 +1,25 @@
+// header.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+
+// 간단한 사운드 출력, 시스템 시간 확인용 (Multimedia system)
+#include <Mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+// DX 관련 헤더 파일
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+// C++ 런타임 헤더 파일
+#include <memory>
+#include <string>
+#include <vector>
