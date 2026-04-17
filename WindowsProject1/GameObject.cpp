3c550d2a0bd0dbf4b2bc30b6fa4dacae7e02265a
laptop-msi <wlgns7922@gmail.com>
2026-04-17 13:58:34 +0900

    오브젝트 간 깊이 검사 기능 추가

--- WindowsProject1/GameObject.cpp
+++ WindowsProject1/GameObject.cpp
@@ -1,4 +1,4 @@
-﻿#include "framework.h"
+#include "framework.h"
 #include "GameObject.h"
 #include "GraphicsPipeline.h"
 
--- WindowsProject1/Scene.cpp
+++ WindowsProject1/Scene.cpp
@@ -1,10 +1,12 @@
-﻿#include "framework.h"
+#include "framework.h"
 #include "Scene.h"
 #include "GraphicsPipeline.h"
+#include "Player.h"
 
 CScene::CScene(CPlayer* pPlayer)
 {
 	m_pPlayer = pPlayer;
+	m_vObjects.push_back(pPlayer);
 }
 
 CScene::~CScene()
@@ -15,62 +17,62 @@ void CScene::BuildObjects()
 {
 	CCubeMesh* pCubeMesh = new CCubeMesh(4.0f, 4.0f, 4.0f);
 
-	m_nObjects = 5;
-	m_ppObjects = new CGameObject * [m_nObjects];
+	CGameObject* newObject = new CGameObject();
+	newObject->SetMesh(pCubeMesh);
+	newObject->SetColor(RGB(255, 0, 0));
+	newObject->SetPosition(-13.5f, 0.0f, +14.0f);
+	newObject->SetRotationAxis(XMFLOAT3(1.0f, 1.0f, 0.0f));
+	newObject->SetRotationSpeed(90.0f);
+	newObject->SetMovingDirection(XMFLOAT3(1.0f, 0.0f, 0.0f));
+	newObject->SetMovingSpeed(0.0f);
+	m_vObjects.push_back(newObject);
 
-	m_ppObjects[0] = new CGameObject();
-	m_ppObjects[0]->SetMesh(pCubeMesh);
-	m_ppObjects[0]->SetColor(RGB(255, 0, 0));
-	m_ppObjects[0]->SetPosition(-13.5f, 0.0f, +14.0f);
-	m_ppObjects[0]->SetRotationAxis(XMFLOAT3(1.0f, 1.0f, 0.0f));
-	m_ppObjects[0]->SetRotationSpeed(90.0f);
-	m_ppObjects[0]->SetMovingDirection(XMFLOAT3(1.0f, 0.0f, 0.0f));
-	m_ppObjects[0]->SetMovingSpeed(0.0f);
+	newObject = new CGameObject();
+	newObject->SetMesh(pCubeMesh);
+	newObject->SetColor(RGB(0, 0, 255));
+	newObject->SetPosition(+13.5f, 0.0f, +14.0f);
+	newObject->SetRotationAxis(XMFLOAT3(0.0f, 1.0f, 1.0f));
+	newObject->SetRotationSpeed(180.0f);
+	newObject->SetMovingDirection(XMFLOAT3(-1.0f, 0.0f, 0.0f));
+	newObject->SetMovingSpeed(0.0f);
+	m_vObjects.push_back(newObject);
 
-	m_ppObjects[1] = new CGameObject();
-	m_ppObjects[1]->SetMesh(pCubeMesh);
-	m_ppObjects[1]->SetColor(RGB(0, 0, 255));
-	m_ppObjects[1]->SetPosition(+13.5f, 0.0f, +14.0f);
-	m_ppObjects[1]->SetRotationAxis(XMFLOAT3(0.0f, 1.0f, 1.0f));
-	m_ppObjects[1]->SetRotationSpeed(180.0f);
-	m_ppObjects[1]->SetMovingDirection(XMFLOAT3(-1.0f, 0.0f, 0.0f));
-	m_ppObjects[1]->SetMovingSpeed(0.0f);
+	newObject = new CGameObject();
+	newObject->SetMesh(pCubeMesh);
+	newObject->SetColor(RGB(0, 255, 0));
+	newObject->SetPosition(0.0f, +5.0f, 20.0f);
+	newObject->SetRotationAxis(XMFLOAT3(1.0f, 0.0f, 1.0f));
+	newObject->SetRotationSpeed(30.15f);
+	newObject->SetMovingDirection(XMFLOAT3(1.0f, -1.0f, 0.0f));
+	newObject->SetMovingSpeed(0.0f);
+	m_vObjects.push_back(newObject);
 
-	m_ppObjects[2] = new CGameObject();
-	m_ppObjects[2]->SetMesh(pCubeMesh);
-	m_ppObjects[2]->SetColor(RGB(0, 255, 0));
-	m_ppObjects[2]->SetPosition(0.0f, +5.0f, 20.0f);
-	m_ppObjects[2]->SetRotationAxis(XMFLOAT3(1.0f, 0.0f, 1.0f));
-	m_ppObjects[2]->SetRotationSpeed(30.15f);
-	m_ppObjects[2]->SetMovingDirection(XMFLOAT3(1.0f, -1.0f, 0.0f));
-	m_ppObjects[2]->SetMovingSpeed(0.0f);
+	newObject = new CGameObject();
+	newObject->SetMesh(pCubeMesh);
+	newObject->SetColor(RGB(0, 255, 255));
+	newObject->SetPosition(0.0f, 0.0f, 40.0f);
+	newObject->SetRotationAxis(XMFLOAT3(0.0f, 0.0f, 1.0f));
+	newObject->SetRotationSpeed(40.6f);
+	newObject->SetMovingDirection(XMFLOAT3(0.0f, 0.0f, 1.0f));
+	newObject->SetMovingSpeed(0.0f);
+	m_vObjects.push_back(newObject);
 
-	m_ppObjects[3] = new CGameObject();
-	m_ppObjects[3]->SetMesh(pCubeMesh);
-	m_ppObjects[3]->SetColor(RGB(0, 255, 255));
-	m_ppObjects[3]->SetPosition(0.0f, 0.0f, 40.0f);
-	m_ppObjects[3]->SetRotationAxis(XMFLOAT3(0.0f, 0.0f, 1.0f));
-	m_ppObjects[3]->SetRotationSpeed(40.6f);
-	m_ppObjects[3]->SetMovingDirection(XMFLOAT3(0.0f, 0.0f, 1.0f));
-	m_ppObjects[3]->SetMovingSpeed(0.0f);
+	newObject = new CGameObject();
+	newObject->SetMesh(pCubeMesh);
+	newObject->SetColor(RGB(128, 0, 255));
+	newObject->SetPosition(10.0f, 10.0f, 50.0f);
+	newObject->SetRotationAxis(XMFLOAT3(0.0f, 1.0f, 1.0f));
+	newObject->SetRotationSpeed(50.06f);
+	newObject->SetMovingDirection(XMFLOAT3(0.0f, 1.0f, 1.0f));
+	newObject->SetMovingSpeed(0.0f);
+	m_vObjects.push_back(newObject);
 
-	m_ppObjects[4] = new CGameObject();
-	m_ppObjects[4]->SetMesh(pCubeMesh);
-	m_ppObjects[4]->SetColor(RGB(128, 0, 255));
-	m_ppObjects[4]->SetPosition(10.0f, 10.0f, 50.0f);
-	m_ppObjects[4]->SetRotationAxis(XMFLOAT3(0.0f, 1.0f, 1.0f));
-	m_ppObjects[4]->SetRotationSpeed(50.06f);
-	m_ppObjects[4]->SetMovingDirection(XMFLOAT3(0.0f, 1.0f, 1.0f));
-	m_ppObjects[4]->SetMovingSpeed(0.0f);
+
 }
 
 void CScene::ReleaseObjects()
 {
-	for (int i = 0; i < m_nObjects; i++) {
-		if (m_ppObjects[i]) delete m_ppObjects[i];
-	}
 
-	if (m_ppObjects) delete[] m_ppObjects;
 }
 
 void CScene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
@@ -109,7 +111,8 @@ void CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wPar
 
 void CScene::Animate(float fElapsedTime)
 {
-	for (int i = 0; i < m_nObjects; i++) m_ppObjects[i]->Animate(fElapsedTime);
+	for (auto& object : m_vObjects)
+		object->Animate(fElapsedTime);
 }
 
 void CScene::Render(HDC hDCFrameBuffer, CCamera* pCamera)
@@ -117,5 +120,16 @@ void CScene::Render(HDC hDCFrameBuffer, CCamera* pCamera)
 	CGraphicsPipeline::SetViewport(&pCamera->m_Viewport);
 	CGraphicsPipeline::SetViewPerspectiveProjectTransform(&pCamera->m_xmf4x4ViewPerspectiveProject);
 
-	for (int i = 0; i < m_nObjects; i++) m_ppObjects[i]->Render(hDCFrameBuffer, pCamera);
+	XMFLOAT4X4 viewMatrix = pCamera->GetViewMatrix();
+
+	std::sort(m_vObjects.begin(), m_vObjects.end(),
+		[&viewMatrix](const CGameObject* a, const CGameObject* b) {
+			float aZ = Vector3::TransformCoord(XMFLOAT3{}, Matrix4x4::Multiply(a->GetWorldMatrix(), viewMatrix)).z;
+			float bZ = Vector3::TransformCoord(XMFLOAT3{}, Matrix4x4::Multiply(b->GetWorldMatrix(), viewMatrix)).z;
+
+			return aZ > bZ;
+		});
+
+	for (const auto& object : m_vObjects)
+		object->Render(hDCFrameBuffer, pCamera);
 }
